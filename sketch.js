c0f3809539b6defc0ab856ca5734f879f3e87ff6
TashaTan1 <136006359+TashaTan1@users.noreply.github.com>
2023-06-09 20:15:05 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function draw() {
       button.position(200, height/2);
       button.mousePressed(function goToAnotherPage() {
         window.location.href =
-          "https://editor.p5js.org/natashatan/sketches/h_e2T2qUD";
+          "https://tashatan1.github.io/visual-six/";
       });
       button = createButton("Let's Breath");
       button.position(425, height/2);
